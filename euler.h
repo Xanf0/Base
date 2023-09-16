1fb08da947a04c166bcc568bf72dc76bddda7a86
Xanf0 <145195439+Xanf0@users.noreply.github.com>
2023-09-16 12:54:34 +0300

    Add files via upload

---
+++ euler.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/*
+* @brief Найдите сумму всех чисел меньше value_, кратных multiples.
+* @param value_: число меньше которого ищем сумму
+* @param multiples_: массив кратных чисел
+* @return сумма чисел
+*/
+unsigned long long int Problem_1(const int value_, const int* multiples_,const unsigned int size_);
